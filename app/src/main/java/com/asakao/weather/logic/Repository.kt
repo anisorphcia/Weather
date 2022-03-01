a148552390d4b146c3a9ad20074d6a1aa305e13f
asakao <zdshen@chinaums.com>
2022-03-01 21:58:15 +0800

    add 天气请求代码

--- app/src/main/java/com/asakao/weather/logic/Repository.kt
+++ app/src/main/java/com/asakao/weather/logic/Repository.kt
@@ -2,9 +2,12 @@ package com.asakao.weather.logic
 
 import androidx.lifecycle.liveData
 import com.asakao.weather.logic.model.Place
+import com.asakao.weather.logic.model.Weather
 import com.asakao.weather.logic.network.SunnyWeatherNetwork
 import kotlinx.coroutines.Dispatchers
-import java.lang.RuntimeException
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlin.RuntimeException
 
 object Repository {
 
@@ -19,6 +22,35 @@ object Repository {
             }
         }catch (e: Exception){
             Result.failure<List<Place>>(e)
+        }
+        emit(result)
+    }
+
+    fun refreshWeather(lng: String, lat: String) = liveData(Dispatchers.IO) {
+        val result = try {
+            coroutineScope {
+                val deferredRealtime = async {
+                    SunnyWeatherNetwork.getRealtimeWeather(lng, lat)
+                }
+                val deferredDaily = async {
+                    SunnyWeatherNetwork.getDailyWeather(lng, lat)
+                }
+                val realtimeResponse = deferredRealtime.await()
+                val dailyResponse = deferredDaily.await()
+                if(realtimeResponse.status == "ok" && dailyResponse.status == "ok"){
+                    val weather = Weather(realtimeResponse.result.realtime, dailyResponse.result.daily)
+                    Result.success(weather)
+                }else{
+                    Result.failure(
+                        RuntimeException(
+                            "realtime response status is ${realtimeResponse.status}" +
+                                    "daily response status is ${dailyResponse.status}"
+                        )
+                    )
+                }
+            }
+        }catch (e: Exception){
+            Result.failure<Weather>(e)
         }
         emit(result)
     }
---
+++ app/src/main/java/com/asakao/weather/logic/network/WeatherService.kt
@@ -0,0 +1,20 @@
+package com.asakao.weather.logic.network
+
+import com.asakao.weather.SunnyWeatherApplication
+import com.asakao.weather.logic.model.DailyResponse
+import com.asakao.weather.logic.model.RealtimeResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface WeatherService {
+
+    @GET("v2.5/${SunnyWeatherApplication.TOKEN}/{lng},{lat}/realtime.json")
+    fun getRealtimeWeather(@Path("lng") lng: String, @Path("lat") lat: String):
+            Call<RealtimeResponse>
+
+    @GET("v2.5/${SunnyWeatherApplication.TOKEN}/{lng}, {lat}/daily.json")
+    fun getDailyWeather(@Path("lng") lng:String, @Path("lat") lat: String):
+            Call<DailyResponse>
+
+}
